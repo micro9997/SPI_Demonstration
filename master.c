74542d1a030d66b7d6224bdf87b11ecc93cb1e5d
Mohanakrishna <micro9997@gmail.com>
2022-12-10 21:38:05 +0530

    with error in master receive

--- master.c
+++ master.c
@@ -4,46 +4,111 @@
 #endif
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #include "readADC.h"
 
 void SPI_MasterInit(void) {
     // Set SCK, MOSI and SS output, all others input
     DDRB |= ((1 << PORTB5) | (1 << PORTB3) | (1 << PORTB2));
-
     // SS to high
     PORTB |= (1 << PORTB2);
-
     // Enable SPI, Master, set clock rate fck/16
     SPCR |= ((1 << SPE) | (1 << MSTR) | (1 << SPR0));
 }
 
-void SPI_MasterTransmit(uint8_t cData) {
+void initLED() {
+    // Set LED pin as output
+    DDRB |= (1 << PORTB1);
+    PORTB &= ~(1 << PORTB1);
+}
+
+void writeLED(uint8_t data) {
+    if(data == 0b00000001) {
+        PORTB |= (1 << PORTB1);
+    } else {
+        PORTB &= ~(1 << PORTB1);
+    }
+}
+
+void sendADC_Data(uint8_t data) {
     // SS low
     PORTB &= ~(1 << PORTB2);
-
     // Start transmission
-    SPDR = cData;
+    SPDR = 0b00000001;
+    // Wait for transmission complete
+    while(!(SPSR & (1 << SPIF)));
+    // Start transmission
+    SPDR = data;
+    // Wait for transmission complete
+    while(!(SPSR & (1 << SPIF)));
+    // SS high
+    PORTB |= (1 << PORTB2);
+}
 
+int8_t readButtonData() {
+    uint8_t correctDataFlag = 0;
+    // Declare the return data
+    uint8_t data = 0;
+    // SS low
+    PORTB &= ~(1 << PORTB2);
+    // Start transmission
+    SPDR = 0b00000010;
+    // Wait for transmission complete
+    while(!(SPSR & (1 << SPIF)));
+    // Start transmission
+    SPDR = 0b00000000;
     // Wait for transmission complete
     while(!(SPSR & (1 << SPIF)));
 
+    // 
+    SPDR = 0b00000000;
+    // Wait for transmission complete
+    while(!(SPSR & (1 << SPIF)));
+    // Validate the data
+    if(SPDR == 0b00000100) {
+        correctDataFlag = 1;
+    }
+    // 
+    SPDR = 0b00000000;
+    // Wait for transmission complete
+    while(!(SPSR & (1 << SPIF)));
+    // 
+    data = SPDR;
+
     // SS high
     PORTB |= (1 << PORTB2);
+
+    if(correctDataFlag == 1) {
+        return data;
+    } else {
+        return -1;
+    }
 }
 
 int main() {
     SPI_MasterInit();
     initADC0();
+    initLED();
 
     uint8_t adcData = 0;
     uint8_t adcDataPre = 0;
+    uint16_t buttonData = 0;
 
     while(1) {
         adcData = readADC0() / 4;
         if(adcData != adcDataPre) {
-            SPI_MasterTransmit(adcData);
+            sendADC_Data(adcData);
             adcDataPre = adcData;
+        }
+        
+        buttonData = readButtonData();
+        if(buttonData != -1) {
+            // writeLED(buttonData);
+            PORTB |= (1 << PORTB1);
+        } else {
+            // Error in SPI communication
+            // PORTB |= (1 << PORTB1);
         }
     }
 
--- slave.c
+++ slave.c
@@ -8,6 +8,8 @@
 
 #include "writePWM.h"
 
+uint8_t buttonData = 1;
+
 void SPI_SlaveInit(void) {
     // Set MISO output, all others input
     DDRB |= (1 << PORTB4);
@@ -28,15 +30,49 @@ uint8_t SPI_SlaveReceive(void) {
 }
 
 ISR(SPI_STC_vect) {
-    writePWM_PB1(SPDR);
+    static uint16_t data = 0;
+    static uint8_t dataLen = 0;
+    static uint8_t needToSend = 0;
+
+    if(needToSend == 1) {
+        SPDR = buttonData;
+        needToSend = 0;
+    } else {
+        dataLen++;
+        if(dataLen == 1) {
+            data = SPDR;
+            SPDR = 0;
+        } else if(dataLen == 2) {
+            data = (data << 8) | SPDR;
+            SPDR = 0;
+
+            if((data & 0b100000000) != 0) {
+                writePWM_PB1((data & 0b11111111));
+            } else if((data & 0b1000000000) != 0) {
+                SPDR = 0b00000100;
+                needToSend = 1;
+            }
+
+            dataLen = 0;
+            data = 0;
+        }
+    }
 }
 
 int main() {
     SPI_SlaveInit();
     initPWM_PB1();
 
-    while(1) {
+    DDRD &= ~(1 << PIND5);
 
+    while(1) {
+        /* if(((PIND & (1 << PIND5)) != 0) && (buttonData == 0)) {
+            buttonData = 1;
+
+        } else if(((PIND & (1 << PIND5)) == 0) && (buttonData == 1)) {
+            buttonData = 0;
+
+        } */
     }
 
     return 0;
